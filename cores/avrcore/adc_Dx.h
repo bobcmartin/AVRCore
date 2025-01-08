7a83dd6e404114bc86505e9a8914b5a091f76db6
Bob Martin <bobdotmartin@gmail.com>
2025-01-07 16:02:08 -0800

    boards.txt typos. ADC core refactor

--- cores/avrcore/adc_Dx.h
+++ cores/avrcore/adc_Dx.h
@@ -6,6 +6,11 @@
 
 */
 
+static const int16_t adc_prescale_to_clkadc[0x0F] PROGMEM = {(F_CPU /   2000L),(F_CPU /   4000L),(F_CPU /  8000L),(F_CPU / 12000L),
+                                                              (F_CPU /  16000L),(F_CPU /  20000L),(F_CPU / 24000L),(F_CPU / 28000L),
+                                                              (F_CPU /  32000L),(F_CPU /  48000L),(F_CPU / 64000L),(F_CPU / 96000L),
+                                                              (F_CPU / 128000L),(F_CPU / 256000L),1};
+
 
 // ADC 0 config
 
@@ -19,11 +24,13 @@ typedef struct ADC0_config
     uint8_t mux_neg;
     uint8_t init_delay;
     uint8_t samp_delay;
+    uint8_t reference;
 
 } ADC0_config_t;
 
-
-
+void analogReference(uint8_t mode);
+void check_valid_analog_pin(pin_size_t pin);
+void check_valid_analog_ref(uint8_t mode);
 void init_ADC0(void);
 
 
--- cores/avrcore/adc_EA.c
+++ cores/avrcore/adc_EA.c
@@ -1,51 +1,67 @@
+/*
 
+    ADC driver for AVR EA C-Nano
+
+    bob martin / microchip dec 2024
+
+
+
+
+
+
+*/
+
+#include "Arduino.h"
 #include "wiring_private.h"
 #include "util/delay.h"
+// #include "adc_Dx.h"         // config inits for ADC0
+#include <avr/pgmspace.h>
 
-#ifndef F_CPU
-  #error "F_CPU not defined. F_CPU must always be defined as the clock frequency in Hz"
-#endif
-#ifndef CLOCK_SOURCE
-  #error "CLOCK_SOURCE not defined. Must be 0 for internal, 1 for crystal, or 2 for external clock"
-#endif
+ #if defined(__AVR_EA__)
+  #pragma message "using AVR EA adc"
+
  
-
- #if defined ((__AVR_EA__))
- #pragma message "using AVR EA adc"
-  void __attribute__((weak)) init_ADC0() 
+void init_ADC0(void) 
   {
     ADC_t* pADC;
-    _fastPtr_d(pADC, &ADC0);    
-      #if F_CPU     > 32000000            // 36 MHz /14 = 2.57 MHz
-        pADC->CTRLB  = ADC_PRESC_DIV10_gc; // 33 MHz /14 = 2.35 MHz
-      #elif F_CPU  >= 30000000            // 32 MHz /12 = 2.67 MHz
-        pADC->CTRLB  = ADC_PRESC_DIV12_gc; // 30 MHz /12 = 2.50 MHz
-      #elif F_CPU  >= 24000000            // 25 MHz /10 = 2.50 MHz
-        pADC->CTRLB  = ADC_PRESC_DIV10_gc; // 24 MHz /10 = 2.40 MHz
-      #elif F_CPU  >= 20000000
-        pADC->CTRLB  = ADC_PRESC_DIV8_gc;  // 20 MHz / 8 = 2.50 MHz
-      #elif F_CPU  >= 16000000
-        pADC->CTRLB  = ADC_PRESC_DIV6_gc;  // 16 MHz / 6 = 2.67 MHz
-      #elif F_CPU  >= 12000000
-        pADC->CTRLB  = ADC_PRESC_DIV4_gc;  // 12 MHz / 4 = 3.00 MHz
-      #elif F_CPU  >=  6000000            // 10 MHz / 4 = 2.50 MHz
-        pADC->CTRLB  = ADC_PRESC_DIV4_gc;  //  8 MHz / 4 = 2.00 MHz
-      #else                               //  5 MHz / 2 = 2.50 MHz
-        pADC->CTRLB  = ADC_PRESC_DIV2_gc;  //  4 MHz / 2 = 2.00 MHz
-      #endif                              //  1 MHz / 2 =  500 kHz
-      pADC->CTRLE = 15; // 15.5 without PGA, 16 with PGA, corresponding to 7.75 or 8 us.
+   
+   
+    adc_config.init_delay = 2;
+    adc_config.left_adjust = false;
+    adc_config.mode = false;          // single ended mode
+
+    analogReference(VDD);       
+   
+    #if F_CPU >= 24000000
+        pADC->CTRLC = ADC_PRESC_DIV20_gc; // 1.2 @ 24, 1.25 @ 25, 1.4 @ 28  MHz
+      #elif F_CPU >= 20000000
+        pADC->CTRLC = ADC_PRESC_DIV16_gc; // 1.25 @ 20 MHz
+      #elif F_CPU >  12000000
+        pADC->CTRLC = ADC_PRESC_DIV12_gc; // 1 @ 12, 1.333 @ 16 MHz
+      #elif F_CPU >= 8000000
+        pADC->CTRLC = ADC_PRESC_DIV8_gc;  // 1-1.499 between 8 and 11.99 MHz
+      #elif F_CPU >= 4000000
+        pADC->CTRLC = ADC_PRESC_DIV4_gc;  // 1 MHz
+      #else                              
+        pADC->CTRLC = ADC_PRESC_DIV2_gc;   // 1 MHz / 2 = 500 kHz
+      #endif
       
-      pADC->CTRLA = ADC_ENABLE_bm | ADC_LOWLAT_bm;
-      pADC->PGACTRL = ADC_PGABIASSEL_75PCT_gc;
-  }
-
-#elif
-  #pragma message "AVR EA ADC block not selected"
-  void __attribute__((weak)) init_ADC0() 
-    {
-        return();
-
-    }
-
-#endif
+      // 16 ADC clock sampling time 
+      pADC->SAMPCTRL = 14; 
+      // VREF init delay
+      pADC->CTRLD = ADC_INITDLY_DLY64_gc; 
+     
+      /* Enable ADC */
+      pADC->CTRLA = ADC_ENABLE_bm | ADC_RESSEL_10BIT_gc;
       
+      #if (defined(__AVR_DA__) && (!defined(NO_ADC_WORKAROUND)))
+        // That may become defined when DA-series silicon is available with the fix
+        pADC->MUXPOS = 0x40;
+        pADC->COMMAND = 0x01;
+        pADC->COMMAND = 0x02;
+      #endif
+  } // end init_ADC0
+
+
+ #endif 
+
